0081dfb3e6a1dce89490d91306e36e84030ec205
Tisaghu <samasher9@hotmail.com>
2024-07-19 16:59:10 -0700

    Removed testing things that will be experimented with more in another branch.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
-# app.py
+from .main_routes import main_bp
+from .test_routes import test_bp
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    # Import and register blueprints
-    from .main_routes import main_bp
-    from .test_routes import test_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(test_bp, url_prefix='/test')
